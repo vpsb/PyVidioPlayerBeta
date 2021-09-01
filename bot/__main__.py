2538e9c16fc89442e5726a465f02fb51b00e347e
Hunter XDD <85411336+Hunter-XDD@users.noreply.github.com>
2021-09-01 12:29:37 +0530

    Umm

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,8 +1,8 @@
-from pyrogram import Client
+from pyrogram import Client as Bot
 from config import API_ID, API_HASH, BOT_TOKEN
 from bot.videoplayer import start
 
-bot = Client(
+bot = Bot(
     ":memory:",
     API_ID,
     API_HASH,
